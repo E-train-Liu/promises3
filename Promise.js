1bc7680317702e9506a49ed8c29036e655703d2d
Yichen LIU <scyyl2@exmail.nottingham.ac.uk>
2020-06-27 06:24:02 +0100

    Finish comments. Fixed async fulfilling.

--- Promise.js
+++ Promise.js
@@ -6,7 +6,6 @@
  * @license MIT
  */
 
-
 (function (definations) {
 
     // CommonJS
@@ -87,29 +86,29 @@
          * @type {boolean}
          */
         var definePropertyUsable;
-        
-        if (!Object.defineProperty)
+        try {
+            Object.defineProperty(new Error(), "test", {
+                get: function() {return []},
+                configurable: true,
+                enumerable: false,
+                writable: true
+            });
+            definePropertyUsable = true;
+        }
+        catch(error) {
             definePropertyUsable = false;
-        else {
-            try {
-                Object.defineProperty(new Error(), "test", {
-                    get: function() {return []},
-                    configurable: true,
-                    enumerable: false,
-                    writable: true
-                });
-                definePropertyUsable = true;
-            }
-            catch(error) {
-                definePropertyUsable = false;
-            }
         }
 
         /**
+         * A bundle of multiple `Error`.
+         * 
+         * An experimental class,
+         * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/AggregateError.
+         * 
          * @class
          * 
-         * @param {Error[]} errors 
-         * @param {string} [message]
+         * @param {Interable} errors  An a interable of errors.
+         * @param {string} [message=""]  The message describing the error. Be `""` on default.
          */
         function AggregateError(errors, message) {
             Error.call(this, message);
@@ -144,10 +143,15 @@
 
 
 
+
+
+
+
+
     /* Member Functions */
 
     Promise.prototype.then = function (onFulfilled, onRejected) {
-        var returnedPromise = new Promise(EMPTY_EXECUTOR);
+        var returnedPromise = new Promise(emptyExecutor);
 
         switch (this["[[status]]"]) {
             case "fulfilled":
@@ -177,11 +181,12 @@
         return this.then(onFinally, onFinally);
     }
 
-    Promise.prototype.toString = function () {
-        // No specification on toString().
-        // But Chrome, Firefox, Edge, Opera and Node.js all act like this.
-        return "[object Promise]";
-    }
+    if (typeof Symbol === "function" && Symbol.toStringTag)
+        Promise.prototype[Symbol.toStringTag] = "Promise";
+
+
+
+
 
 
 
@@ -229,150 +234,13 @@
     }
 
 
-    /**
-     * 
-     * @param {any[]} promises 
-     * @param {(index: number, value: any) => void} [onFulfilledAt]
-     * @param {(index: number, reason: any) => void} [onRejectedAt]
-     */
-    function watchPromises(promises, onFulfilledAt, onRejectedAt) {
-        var promiseCount = promises.length;
-
-        for (var i = 0; i < promiseCount; ++i) {
-            var promise = promises[i];
-            try {
-                if (promise != null && typeof promise.then === "function")
-                    promise.then(
-                        curryBinary(onFulfilledAt, i),
-                        curryTenary(onRejectedAt, i)
-                    );
-                else
-                    invokeFunctionAsync(onFulfilledAt, i, promise);
-            }
-            catch(error) {
-                invokeFunctionAsync(onRejectedAt, i, error);
-            }
-        }
-    }
-
-    /**
-     * 
-     * @param {any[]} promises 
-     * @param {(value: any[]) => void} resolvePromise
-     * @param {(reason: any) => void} rejectPromise 
-     */
-    function allExecutor(promises, resolvePromise, rejectPromise) {
-        var unfulfilledCount = promises.length;
-        var values = new Array(promises.length);
-        var rejected = false;
-
-        if (unfulfilledCount === 0) {
-            resolvePromise(values);
-            return;
-        }
-
-        function onFulfilledAt(index, value) {
-            values[index] = value;
-            if (--unfulfilledCount <= 0)
-                resolvePromise(values);
-        }
-        function onRejectedAt(index, reason) {
-            if (!rejected) {
-                rejected = true;
-                rejectPromise(reason);
-            }
-        }
-        watchPromises(onFulfilledAt, onRejectedAt);
-    }
-
-    /**
-     * 
-     * @param {any[]} promises 
-     * @param {(value: any[]) => void} resolvePromise
-     */
-    function allSettledExecutor(promises, resolvePromise, rejectPromise) {
-        var pendingCount = promises.length;
-        var values = new Array(promises.length);
-
-        if (pendingCount === 0) {
-            resolvePromise(values)
-            return;
-        }
-
-        function onSettledAt(index, value) {
-            values[index] = value;
-            if (--pendingCount <= 0)
-                resolvePromise(values);
-        }
-        watchPromises(promises, onSettledAt, onSettledAt);
-    }
-
-    /**
-     * 
-     * @param {Promise} promises 
-     * @param {(value: any) => void} onOneFulfilled 
-     * @param {(reason: AggregateError) => void} onAllRejected 
-     */
-    function anyExecutor(promises, onOneFulfilled, onAllRejected) {
-        var unrejectedCount = promises.length;
-        var reasons = new Array(promises.length);
-        var fulfilled = false;
-
-        if (unrejectedCount === 0) {
-            onAllRejected(new AggregateError(reasons));
-            return;
-        }
-
-        function onFulfilledAt(index, value) {
-            if (!fulfilled) {
-                fulfilled = true;
-                onOneFulfilled(value);
-            }
-        }
-        function onRejectedAt(index, reason) {
-            reasons[index] = reason;
-            if (--unrejectedCount <= 0)
-                onAllRejected(new AggregateError(reasons));
-        }
-        watchPromises(onFulfilledAt, onRejectedAt);
-    }
 
 
-    function raceExecutor(promises, resolvePromise, rejectPromise) {
-        var settled = false;
 
-        function onSettledAt(index, value) {
-            if (!settled) {
-                settled = true;
-                resolvePromise(value);
-            }
-        }
-        watchPromises(promises, onSettledAt, onSettledAt);
-    }
 
-    // If ES5 or above, take advantage of Object.defineProperty().
-    // Config all functions to be unenumerable, which is they should be in ES6.
-    // For IE8, Object.defineProperty() exists but will throw an error when
-    // being applied on non-DOM objects.
-    try {
-        // Standard descriptor for ES6 member functions.
-        var descriptor = {
-            value: null,
-            configurable: true,
-            enumerable: false,
-            writeable: true
-        };
 
-        for (var k in Promise) {
-            descriptor.value = Promise[k];
-            Object.defineProperty(Promise, k, descriptor);
-        }
-        for (var k in Promise.prototype) {
-            descriptor.value = Promise.prototype[k];
-            Object.defineProperty(Promise.prototype, k, descriptor);
-        }
-    }
-    catch (error) { }
+
+    /* Helper Functions For Member Functions */
 
     /**
      * Resolve a promise.
@@ -429,15 +297,13 @@
     }
 
     /**
+     * Fulfill a pending promise.
      * 
      * @param {Promise} promise 
      * @param {any} value 
      */
     function fulfill(promise, value) {
-        // When fulfill the promise:
-        // + Set its status to "fulfilled".
-        // + Set its value.
-        // + Call all onFulfilled callback (added by promise.then()).
+        
         if (promise["[[status]]"] !== "pending")
             return;
 
@@ -455,6 +321,9 @@
     }
 
     /**
+     * Invoke an onFulfilled callback added by then().
+     * 
+     * When `promise1.then()` was called, it will accept a 
      * 
      * @param {any | (value: any) => any} onFulfilled 
      * @param {any} value 
@@ -462,9 +331,14 @@
      */
     function invokeOnFulFilledCallback(onFulfilled, value, returnedPromise) {
         if (typeof onFulfilled === "function")
+            // https://promisesaplus.com/#point-34
+            //      The `onFulfilled` and `onRejected` callbacks must be called async.
             invokeFunctionAsync(
                 invokeThenCallback, onFulfilled, result, returnedPromise
             );
+        // https://promisesaplus.com/#point-43
+        //      If `onFulfilled` is not a function. Resolve the next promise with
+        //      the value of this promise.
         else
             resolve(returnedPromise, value);
     }
@@ -505,6 +379,8 @@
     }
 
     /**
+     * Check if the `onRejected` callback accepted by `then()` is a function.
+     * If it is, call it.
      * 
      * @param {any | (value: any) => any} onFulfilled 
      * @param {any} reason 
@@ -512,15 +388,24 @@
      */
     function invokeOnRejectedCallback(onRejected, reason, returnedPromise) {
         if (typeof onRejected === "function")
+            // https://promisesaplus.com/#point-34
+            //      The `onFulfilled` and `onRejected` callbacks must be called async.
             invokeFunctionAsync(
                 invokeThenCallback, onRejected, reason, returnedPromise
             );
+        // https://promisesaplus.com/#point-44
+        //      If `onRejected` is not a function. Reject the next promise with
+        //      the value of this promise.
         else
             reject(returnedPromise, reason);
     }
 
 
     /**
+     * Invoke the `onFulfilled` or `onRejected` callback added by `then()`.
+     * 
+     * It will resolve or reject the next `Promise` returned by `then()` according
+     * to the running result of the callback.
      * 
      * @param {(value: any) => void} callback 
      * @param {any} value 
@@ -528,13 +413,185 @@
      */
     function invokeThenCallback(callback, value, returnedPromise) {
         try {
+            // https://promisesaplus.com/#point-41
+            //      When `onFulfilled` or `onRejected` return a value, resolve
+            //      the `Promise` returned by `then()` with the return value.
             var callbackReturnValue = callback(value);
             resolve(returnedPromise, callbackReturnValue)
         }
         catch (error) {
+            // https://promisesaplus.com/#point-42
+            //      When `onFulfilled` or `onRejected` throws an exception, 
+            //      reject the next `Promise`.
             reject(returnedPromise, error);
         }
     }
+
+
+
+
+
+
+
+
+    /* Helper Functions for Static Functions */
+
+    /**
+     * To watch an array of `Promise`s, add the same fulfill and reject
+     * callback by `then()` on them.
+     * 
+     * If an element in the array with value `v` is not a promise, it will be seen as
+     * an promise being fulfilled with `v`.   
+     * 
+     * @param {any[]} promises 
+     * @param {(index: number, value: any) => void} [onFulfilledAt]
+     * @param {(index: number, reason: any) => void} [onRejectedAt]
+     */
+    function watchPromises(promises, onFulfilledAt, onRejectedAt) {
+        var promiseCount = promises.length;
+
+        for (var i = 0; i < promiseCount; ++i) {
+            var promise = promises[i];
+            try {
+                if (promise != null && typeof promise.then === "function")
+                    promise.then(
+                        curryBinary(onFulfilledAt, i),
+                        curryTenary(onRejectedAt, i)
+                    );
+                else
+                    onFulfilledAt(i, promise);
+            }
+            catch(error) {
+                onRejectedAt(i, error);
+            }
+        }
+    }
+
+    /**
+     * The executor to create the promise returned by `Promise.all()`.
+     * 
+     * @param {any[]} promises 
+     * @param {(value: any[]) => void} resolvePromise
+     * @param {(reason: any) => void} rejectPromise 
+     */
+    function allExecutor(promises, resolvePromise, rejectPromise) {
+        var unfulfilledCount = promises.length;
+        var values = new Array(promises.length);
+        var rejected = false;
+
+        if (unfulfilledCount === 0) {
+            resolvePromise(values);
+            return;
+        }
+
+        function onFulfilledAt(index, value) {
+            values[index] = value;
+            if (--unfulfilledCount <= 0)
+                // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all#Fulfillment
+                //      If all promises are fulfilled, the promise returned by `Promise.all()` should be fullfilled async.
+                invokeFunctionAsync(resolvePromise, values);
+        }
+        function onRejectedAt(index, reason) {
+            if (!rejected) {
+                rejected = true;
+                invokeFunctionAsync(rejectPromise, reason);
+            }
+        }
+        watchPromises(onFulfilledAt, onRejectedAt);
+    }
+
+    /**
+     * The executor to create the promise returned by `Promise.allSettled()`.
+     * 
+     * @param {any[]} promises 
+     * @param {(value: any[]) => void} resolvePromise
+     */
+    function allSettledExecutor(promises, resolvePromise, rejectPromise) {
+        var pendingCount = promises.length;
+        var values = new Array(promises.length);
+
+        if (pendingCount === 0) {
+            resolvePromise(values)
+            return;
+        }
+
+        function onSettledAt(index, value) {
+            values[index] = value;
+            if (--pendingCount <= 0)
+                // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/allSettled#Return_value
+                invokeFunctionAsync(resolvePromise, values);
+        }
+        watchPromises(promises, onSettledAt, onSettledAt);
+    }
+
+    /**
+     * The executor to create the promise returned by `Promise.any()`.
+     * 
+     * @param {Promise} promises 
+     * @param {(value: any) => void} onOneFulfilled 
+     * @param {(reason: AggregateError) => void} onAllRejected 
+     */
+    function anyExecutor(promises, onOneFulfilled, onAllRejected) {
+        var unrejectedCount = promises.length;
+        var reasons = new Array(promises.length);
+        var fulfilled = false;
+
+        if (unrejectedCount === 0) {
+            onAllRejected(new AggregateError(reasons));
+            return;
+        }
+
+        function onFulfilledAt(index, value) {
+            if (!fulfilled) {
+                fulfilled = true;
+                invokeFunctionAsync(onOneFulfilled, value);
+            }
+        }
+        function onRejectedAt(index, reason) {
+            reasons[index] = reason;
+            if (--unrejectedCount <= 0)
+                invokeFunctionAsync(onAllRejected, new AggregateError(reasons));
+        }
+        watchPromises(onFulfilledAt, onRejectedAt);
+    }
+
+    /**
+     * The executor to create the promise returned by `Promise.race()`.
+     * 
+     * @param {Promise} promises 
+     * @param {(value: any) => void} onOneFulfilled 
+     * @param {(reason: AggregateError) => void} onAllRejected 
+     */
+    function raceExecutor(promises, resolvePromise, rejectPromise) {
+        var settled = false;
+
+        function onSettledAt(index, value) {
+            if (!settled) {
+                settled = true;
+                invokeFunctionAsync(resolvePromise, value);
+            }
+        }
+        watchPromises(promises, onSettledAt, onSettledAt);
+    }
+
+    /**
+     * A executor function for `Promise` which do nothing.
+     * 
+     * By using this excutor, you can create a `Promise` which keeps pending.
+     * 
+     * @param {(value: any) => void} resolve 
+     * @param {(reason: any) => void} reject 
+     */
+    function emptyExecutor(resolve, reject) { }
+
+
+
+
+
+
+
+
+    /* Common Helper Functions */
 
     /**
      * Call a function asynchronizely.
@@ -544,13 +601,11 @@
      * @param {...any} args
      */
     function invokeFunctionAsync(func, args) {
-        // Optimization for function with 0-3 argument.s
+        // Optimization for function with 0, 1 and 3 argument(s) 
+        // since we only use these 3 conditions. 
         switch(arguments.length) {
             case 4:
                 setTimeout(func, 0, arguments[1], arguments[2], arguments[3]);
-                break;
-            case 3:
-                setTimeout(func, 0, arguments[1], arguments[2]);
                 break;
             case 2:
                 setTimeout(func, 0, arguments[1]);
@@ -566,17 +621,17 @@
                 setTimeout.apply(this, argArray);
         }
     }
-    
-
-
-
-
 
     /**
+     * Curry a function which takes 2 parameter and fix its first argument.
+     * Return the function which only need the 2nd argument. 
      * 
-     * @param {(arg1: T1, arg2: T2) => T0} func 
-     * @param {T1} arg1
-     * @returns {(arg2: T2) => T0}
+     * For currying, see https://en.wikipedia.org/wiki/Currying and https://javascript.info/currying-partials.
+     * 
+     * @param {(arg1: T1, arg2: T2) => T0} func  A function which requires 2 arguments.
+     * @param {T1} arg1  The first argument to be passed to `func`.
+     * @returns {(arg2: T2) => T0}  A new function will takes 1 argument. 
+     *                              Calling it with `arg2` is equivalent to calling `func` with `(arg1, arg2)`
      */
     function curryBinary(func, arg1) {
         return function (arg2) {
@@ -585,10 +640,13 @@
     }
 
     /**
+     * Curry a function which takes 3 parameter and fix its first argument.
+     * Return the function which only need the 2 later arguments
      * 
-     * @param {(arg1: T1, arg2: T2, arg3: T3) => T0} func 
-     * @param {T1} arg1
-     * @returns {(arg2: T2, arg3: T3) => T0}
+     * @param {(arg1: T1, arg2: T2, arg3: T3) => T0} func  A function which requires3 arguments.
+     * @param {T1} arg1  The first argument to be passed to `func`.
+     * @returns {(arg2: T2, arg3: T3) => T0}  A new function will takes 1 argument. 
+     *                                        Calling it with `arg2` is equivalent to calling `func` with `(arg1, arg2)`
      */
     function curryTenary(func, arg1) {
         return function (arg2, arg3) {
@@ -597,6 +655,10 @@
     }
 
     /**
+     * Convert an iterable object to an array.
+     * 
+     * See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols.
+     * 
      * @function
      * @type {(iterable: Interable) => Array}
      * 
@@ -642,7 +704,13 @@
     }
 
 
-    function EMPTY_EXECUTOR(resolve, reject) { }
+
+
+
+
+
+    
+    /* Export */
 
     return {
         Promise: Promise,
