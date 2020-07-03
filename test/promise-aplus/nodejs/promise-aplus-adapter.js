05b8f1495c5ef485ee3e0ff82d244a7f93cdf849
Yichen LIU <scyyl2@exmail.nottingham.ac.uk>
2020-07-03 14:09:25 +0100

    Add Promise A+ test in nodejs.

---
+++ test/promise-aplus/nodejs/promise-aplus-adapter.js
@@ -0,0 +1,28 @@
+/**
+ * Define a module as adapter for promise-aplus-tests.
+ */
+
+
+const promises3 = require("../../../Promise.js");
+
+function deferred() {
+    let resolve, reject;
+    let promise = new promises3.Promise(
+        function (_resolve, _reject) {
+            resolve = _resolve;
+            reject = _reject;
+        }
+    );
+
+    return {
+        resolve: resolve,
+        reject: reject,
+        promise: promise
+    };
+}
+
+module.exports = {
+    resolve: promises3.Promise.resolve,
+    reject: promises3.Promise.reject,
+    deferred: deferred
+};
